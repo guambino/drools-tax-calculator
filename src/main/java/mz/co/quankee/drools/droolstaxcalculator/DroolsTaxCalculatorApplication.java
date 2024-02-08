4e8d9d6f065f4705905842f202d41fb0ff8f49a5
Guambino <Ivan.Guambe@fnb.co.mz>
2024-02-08 18:46:02 +0200

    Source code added

---
+++ src/main/java/mz/co/quankee/drools/droolstaxcalculator/DroolsTaxCalculatorApplication.java
@@ -0,0 +1,13 @@
+package mz.co.quankee.drools.droolstaxcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsTaxCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsTaxCalculatorApplication.class, args);
+	}
+
+}
